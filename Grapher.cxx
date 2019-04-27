286b23b08289b2e6cec517f9f1f971a1c5ef1a31
pattonsgirl <pattonsgirl@gmail.com>
2019-04-26 22:26:49 -0400

    Fixed data files tab delimination,
    code to read columns and rows as vtkTable

--- Grapher.cxx
+++ Grapher.cxx
@@ -5,6 +5,9 @@
 #include <vtkSmartPointer.h>
 
 #include <vtkDelimitedTextReader.h>
+#include <vtkTableToGraph.h>
+#include <vtkTable.h>
+
 
 //includes for C++
 #include <string>
@@ -15,22 +18,36 @@ using namespace std;
 
 //int main(int, char* [])
 int main(int argc, char* argv[]) {
+    //string inputFilename = "../data/Flitter_Names.txt";
+
     //setting up file readers - data can be interpreted as csv files
+    //formatted as numeric id, Flitter handle
+    //These are the vertex names
     auto *flitter_names = vtkDelimitedTextReader::New();
-    flitter_names->SetFileName("data/Flitter Names.txt");
-    //flitter_names->DetectNumericColumnsOn();
+    flitter_names->SetFileName("../data/Flitter_Names.txt");
+    flitter_names->DetectNumericColumnsOn();
     flitter_names->SetFieldDelimiterCharacters("\t");
-    //flitter_names->Update();
+    flitter_names->Update();
 
+    //formatted as numeric id, friend ID
+    //These are the vertex edges
     auto *people_cities = vtkDelimitedTextReader::New();
-    people_cities->SetFileName("data/People-Cities.txt");
+    people_cities->SetFileName("../data/People-Cities.txt");
     people_cities->SetFieldDelimiterCharacters("\t");
-    //people_cities->Update();
+    people_cities->Update();
 
+    //formatted as numeric id, City
+    //Cluster as geo locations?
     auto *links_table = vtkDelimitedTextReader::New();
-    links_table->SetFileName("data/Links_Table.txt");
+    links_table->SetFileName("../data/Links_Table.txt");
     links_table->SetFieldDelimiterCharacters("\t");
-    //links_table->Update();
+    links_table->Update();
+
+    vtkTable* table = people_cities->GetOutput();
+    std::cout << "Table has " << table->GetNumberOfRows()
+            << " rows." << std::endl;
+    std::cout << "Table has " << table->GetNumberOfColumns()
+            << " columns." << std::endl;
 
     
 
